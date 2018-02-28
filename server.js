a7f0eb6251715dcf35509bf6b066d6e4fb0ebdc6
Subhasish Saha <30802192+Subha-001@users.noreply.github.com>
2018-02-28 16:14:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function createTemplate (data){
 var title=data.title;
 var date=data.date;
 var heading=data.heading;
-var cntent=data.content;
+var content=data.content;
 
 var htmlTemplate=`
  <html>
