78d53bac7961a2be363da8ef7d3d6b7966087994
Subhasish Saha <30802192+Subha-001@users.noreply.github.com>
2018-03-01 12:14:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
-var img = document.getElementById('img');
+var img = document.getElementById('madi.png');
 img.onclick=function(){
     img.style.margineLeft='100px';
     
