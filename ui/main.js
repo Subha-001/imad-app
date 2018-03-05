871aa496bb96ec4edb044d34903f783fed810860
Subhasish Saha <30802192+Subha-001@users.noreply.github.com>
2018-03-05 15:38:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,24 @@
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
-button.onclick = function() {
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-}
+
+button.onclick = function() { 
+    
+    //create a request object
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+       
+        if(request.readystate === XMLHttpRequest.DONE){
+             
+             if(request.status===200){
+             var counter = request.responseText;
+             var span =document.getElementById('count');
+             span.innerHTML=counter.toString();
+             }
+        }
+    };   
+    
+     //make the request
+    request.open('GET','http://http://amisubhasish100.imad.hasura-app.io//counter',true);
+    request.send(null);
+};
