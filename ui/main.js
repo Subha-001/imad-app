f08295f1045bd4c6c235543b5cf0465fa2e19103
Subhasish Saha <30802192+Subha-001@users.noreply.github.com>
2018-03-14 01:07:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,10 @@
 //counter code
 var button=document.getElementById('counter');
+          
 
 button.onclick = function() { 
     
-   
-    
-    //create a request object
+   //create a request object
     var request=new XMLHttpRequest();
     
     request.onreadystatechange = function(){
@@ -13,28 +12,22 @@ button.onclick = function() {
         if(request.readystate === XMLHttpRequest.DONE){
              
              if(request.status===200){
-             var counter = request.responseText;
-             var span =document.getElementById('count');
-             span.innerHTML=counter.toString();
+              var names = request.responseText;
+              names=JSON.parse(names);
+              var list='';
+              for(var i=0;i<names.length;i++){
+              list+='<li>' + names[i] + '</li>';
+           }
+        var ul=document.getElementById('namelist');
+        ul.innerHTML=list;
              }
         }
     };   
     
      //make the request
-    request.open('GET', 'http://amisubhasish100.imad.hasura-app.io/counter',true);
+    request.open('GET', 'http://amisubhasish100.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
+
+
 //submit name
-var nameinput = document.getElementById('name');
-var name = nameinput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick =function(){
-     var names = ['name1','name2','name3','name4'];
-     var list='';
-     for(var i=0;i<names.length;i++){
-         list+='<li>' + names[i] + '</li>';
-         
-     }
-     var ul=document.getElementById('namelist');
-     ul.innerHTML=list;
-};
