373158911df1cec499473d4e3f7cf2ca8cd99e32
Subhasish Saha <30802192+Subha-001@users.noreply.github.com>
2018-03-14 01:49:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 //counter code
 var button=document.getElementById('counter');
 
-
 button.onclick = function() { 
     
    //create a request object
@@ -36,7 +35,7 @@ submit.onclick = function() {
         if(request.readystate === XMLHttpRequest.DONE){
              
              if(request.status===200){
-              var names = request.responseText;
+              var names = request.ResponseText;
               names=JSON.parse(names);
               var list='';
               for(var i=0;i<names.length;i++){
