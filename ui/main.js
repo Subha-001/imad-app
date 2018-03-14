420e08129ff68b194e4deb1095ff1630d0a30168
Subhasish Saha <30802192+Subha-001@users.noreply.github.com>
2018-03-14 15:55:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,11 +23,9 @@ button.onclick = function() {
 };
 
 
- var nameinput=document.getElementById('name');
-     var name = nameinput.value;
+
 
 var submit=document.getElementById('submit_btn');
-
 submit.onclick = function() { 
     
    //create a request object
@@ -41,7 +39,7 @@ submit.onclick = function() {
               var names = request.responseText;
               names=JSON.parse(names);
               var list=' ';
-              for(var i=0;i<names.length;i++){
+              for(var i=0; i<names.length; i++){
               list+='<li>' + names[i] + '</li>';
            }
         var ul=document.getElementById('namelist');
@@ -51,7 +49,8 @@ submit.onclick = function() {
     };   
     
      //make the request
-    
+     var nameinput=document.getElementById('name');
+     var name = nameinput.value;
     request.open('GET', 'http://amisubhasish100.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
